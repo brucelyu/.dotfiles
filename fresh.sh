6e79b8c090d7ba4ffa0f5d817e5a20a200a225f8
Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
2022-06-04 22:31:19 +0900

    fix a syntax error

--- fresh.sh
+++ fresh.sh
@@ -3,13 +3,13 @@
 echo "Setting up your OS..."
 
 # Check for Oh My Zsh and install if we don't have it
-if [ -d ~/.oh-my-zsh ]; then
+if [[ -d ~/.oh-my-zsh ]]; then
     echo "oh-my-zsh is installed"
 else
-    if [ "$(uname)" == "Darwin" ]; then
+    if [[ "$(uname)" == "Darwin" ]]; then
         # Do something under Mac OS X platform
         sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    elif [ "$(uname)" == "Linux" ]; then
+    elif [[ "$(uname)" == "Linux" ]]; then
         # Do something under GNU/Linux platform
         sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
     fi
