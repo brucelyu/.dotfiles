ef0a6cb1d4d985f116fb3ef2bdbd03642d184cdd
Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
2022-06-05 14:49:05 +0900

    add alias and path for zsh

---
+++ aliases.zsh
@@ -0,0 +1,3 @@
+# For cluster to launch interactive nodes
+alias intactnode='srun -N 1 -c 4 -p gr4 --pty bash -i'
+alias jupytercluster="jupyter-notebook --no-browser --port=8889 --ip=0.0.0.0"
---
+++ path.zsh
@@ -0,0 +1,5 @@
+# Add personal developed python packages folder to
+# Python path
+PATH=$PATH:$HOME/.local/bin
+PYTHONPATH="$HOME/Codes:$PYTHONPATH"
+export PYTHONPATH
