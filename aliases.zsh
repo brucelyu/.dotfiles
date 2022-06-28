fe3b2d23854d39e220c54c05830c016983f21a54
Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
2022-06-28 21:41:18 +0900

    interact nodes in cluster using less cores

--- aliases.zsh
+++ aliases.zsh
@@ -1,3 +1,3 @@
 # For cluster to launch interactive nodes
-alias intactnode='srun -N 1 -c 2 -p gr4 --pty bash -i'
+alias intactnode='srun -N 1 -c 2 -p gr3 --pty bash -i'
 alias jupytercluster="jupyter-notebook --no-browser --port=8889 --ip=0.0.0.0"
