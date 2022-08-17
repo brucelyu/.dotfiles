5a139c5485e3d00994b90a3428d952ca1487d1b0
Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
2022-08-17 14:29:17 +0900

    add tmux configuration files and alias

--- aliases.zsh
+++ aliases.zsh
@@ -6,6 +6,12 @@ alias jupytercluster="jupyter-notebook --no-browser --port=8889 --ip=0.0.0.0"
 alias sshfsreims="sshfs lyu@reims.issp.u-tokyo.ac.jp:/home/lyu/ $HOME/workstation -ovolname=reims"
 alias sshfsohtaka="sshfs k014821@ohtaka.issp.u-tokyo.ac.jp:/home/k0148/k014821/ $HOME/systemb-issp -ovolname=ohtaka"
 
+# tmux aliases
+alias ta='tmux attach'
+alias tls='tmux ls'
+alias tat='tmux attach -t'
+alias tns='tmux new-session -s'
+
 # For listen to ports for reims cluster
 sshlisten(){ssh -N -f -L $1:"$2":$1 lyu@reims.issp.u-tokyo.ac.jp}
 sshkill(){lsof -ti tcp:$1 | xargs kill -9}
