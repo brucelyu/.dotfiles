60580d49ba990fcfa089158dac3980b2f99a947c
Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
2023-04-28 13:38:11 +0900

    script for downloading videos

--- aliases.zsh
+++ aliases.zsh
@@ -18,6 +18,8 @@ alias dev-tns='tmux new-session \; \
   resize-pane -D 20 \; \
   split-window -h \; ' 
 alias physcd='cd ~/OneDrive\ -\ The\ University\ of\ Tokyo/Physics/'
+# download video as mp4 using cookies from chrome (For high quality videos)
+yt-mp4-dl(){yt-dlp $1 --cookies-from-browser chrome -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'}
 
 # For listen to ports for reims cluster
 sshlisten(){ssh -N -f -L $1:"$2":$1 lyu@reims.issp.u-tokyo.ac.jp}
